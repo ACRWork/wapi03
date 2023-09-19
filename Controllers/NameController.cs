22203da65aba54e8d69acc1a077b02d2306a7b10
Faik Hakan Bilgen <faikb@microsoft.com>
2023-09-19 17:14:38 +0300

    added name controller

---
+++ Controllers/NameController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wapi03.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : Controller
+    {
+        private static readonly string[] Names = new[]
+       {
+        "Red", "Green", "Blue", "Black", "White"
+        };
+
+        private readonly ILogger<NameController> _logger;
+
+        public NameController(ILogger<NameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetName")]
+        public IEnumerable<string> Get()
+        {
+            return Names;
+        }
+    }
+}
