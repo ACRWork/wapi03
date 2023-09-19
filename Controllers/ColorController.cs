8c81ccb9f223174fb3efdf564cb4417013dbb98f
Faik Hakan Bilgen <faikb@microsoft.com>
2023-09-19 14:06:17 +0300

    new controller

---
+++ Controllers/ColorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wapi03.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ColorController : Controller
+    {
+        private static readonly string[] Colors = new[]
+        {
+        "Red", "Green", "Blue", "Black", "White"
+        };
+
+        private readonly ILogger<ColorController> _logger;
+
+        public ColorController(ILogger<ColorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetColor")]
+        public IEnumerable<string> Get()
+        {
+            return Colors;
+        }
+    }
+}
